8e81b37bb5032d81dfd72bec136e26e3dc12b45a
Thanh Long <thanhlongnosuke@gmail.com>
2024-12-21 13:56:38 +0700

    Create Listings.jsx

---
+++ src/components/Listings.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Listings = ({ locations, loading, mapRef, markers }) => {
+  const handleListingClick = (location) => {
+    mapRef.current.flyTo({
+      center: [location.lng, location.lat],
+      zoom: 16,
+    });
+
+    const marker = markers.find(
+      (m) =>
+        m.getLngLat().lng === location.lng && m.getLngLat().lat === location.lat
+    );
+
+    if (marker) marker.togglePopup();
+  };
+
+  return (
+    <div id="listings" className="listings">
+      {loading ? (
+        <div>Loading locations...</div>
+      ) : (
+        locations.map((location, index) => (
+          <div
+            key={index}
+            className="listing"
+            onClick={() => handleListingClick(location)}
+          >
+            <h2>{location.name}</h2>
+            <p>
+              <strong>Wi-Fi:</strong> {location.wifi}
+            </p>
+            <p>
+              <strong>House Number:</strong> {location.house_number}
+            </p>
+            <p>
+              <strong>Address:</strong> {location.address}
+            </p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Listings;
