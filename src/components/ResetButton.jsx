6465c44740ee7f376d783872fd036a956d6b45d9
Thanh Long <thanhlongnosuke@gmail.com>
2024-12-21 13:56:42 +0700

    Create ResetButton.jsx

---
+++ src/components/ResetButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ResetButton = ({ onClick }) => (
+  <button className="reset-button" onClick={onClick}>
+    Reset
+  </button>
+);
+
+export default ResetButton;
